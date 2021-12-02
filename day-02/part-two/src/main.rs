5a6a34f126eb694f83551d7306ca543c641c4c33
Damian Karczewski <damian.karczewski1@gmail.com>
2021-12-02 22:05:45 +0100

    Day-02: Part two

---
+++ day-02/part-two/src/main.rs
@@ -0,0 +1,122 @@
+use std::convert::TryFrom;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+mod commands {
+    pub const FORWARD: &str = "forward";
+    pub const DOWN: &str = "down";
+    pub const UP: &str = "up";
+}
+
+// The structure describes the pilot movement pattern. It contains
+// information about how much it moves.
+//
+// Movement depends on the `commands`.
+struct MovementScheme {
+    horizontal: isize,
+    aim: isize,
+}
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() != 2 {
+        eprintln!("Program must be executed with one argument: [file_name]");
+        return;
+    }
+
+    // First argument is name of binary file.
+    // Usefully is second argument which is `file name` with data.
+    let file_name = match args.get(1) {
+        Some(name) => name,
+        None => {
+            eprintln!("unable to get file name");
+            return;
+        }
+    };
+
+    let path = Path::new(file_name);
+    let file = match File::open(path) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("unable to open file, {}", e);
+            return;
+        }
+    };
+
+    let reader = BufReader::new(file);
+    let mut horizontal_position = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+        match MovementScheme::try_from(line) {
+            Ok(scheme) => {
+                horizontal_position += scheme.horizontal;
+                aim += scheme.aim;
+                depth += if aim != 0 { scheme.horizontal * aim } else { 0 };
+            }
+            Err(e) => {
+                eprintln!("{}", e);
+                return;
+            }
+        }
+    }
+    println!(
+        "Horizontal position {}, depth {}, product {}",
+        horizontal_position,
+        depth,
+        horizontal_position * depth
+    );
+}
+
+impl TryFrom<String> for MovementScheme {
+    type Error = String;
+
+    fn try_from(line: String) -> Result<Self, Self::Error> {
+        let split_line = line.split_ascii_whitespace().collect::<Vec<&str>>();
+        // Acceptable format: '[command] + [movement]' (two items).
+        if split_line.len() != 2 {
+            let message = format!("incorrect data in file: '{:?}'", split_line);
+            return Err(message);
+        }
+        let movement = split_line[1].parse::<isize>().unwrap();
+        let (horizontal, aim) = match split_line[0] {
+            commands::FORWARD => (movement, 0),
+            commands::DOWN => (0, movement),
+            #[allow(clippy::neg_multiply)]
+            commands::UP => (0, -1 * movement),
+            _ => {
+                let message = format!("unknown command: '{:?}'", split_line[0]);
+                return Err(message);
+            }
+        };
+        Ok(MovementScheme { horizontal, aim })
+    }
+}
+
+#[cfg(test)]
+mod example_data {
+    #[test]
+    fn example_data() {
+        let input = vec![
+            "forward 5".to_owned(),
+            "down 5".to_owned(),
+            "forward 8".to_owned(),
+            "up 3".to_owned(),
+            "down 8".to_owned(),
+            "forward 2".to_owned(),
+        ];
+        let mut horizontal_position = 0;
+        let mut depth = 0;
+        let mut aim = 0;
+        for line in input {
+            let scheme = super::MovementScheme::try_from(line).unwrap();
+            horizontal_position += scheme.horizontal;
+            aim += scheme.aim;
+            depth += if aim != 0 { scheme.horizontal * aim } else { 0 };
+        }
+        assert_eq!(horizontal_position * depth, 900);
+    }
+}
